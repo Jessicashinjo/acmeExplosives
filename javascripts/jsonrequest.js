493f40741379942df58c1e25bde66d6abf23472a
Jessica Wynn <Jessicashinjo@gmail.com>
2016-05-13 15:58:27 -0500

    basic html interface complete and promises finished

---
+++ javascripts/jsonrequest.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var productInfo = (function(productInfo){
+  var categories = [];
+  var types = [];
+  var products = [];
+
+  function categoriesPromise(){
+    return new Promise(function(resolve, reject){
+      $.ajax("/javascripts/categories.json")
+        .done(function(data){
+          resolve(data);
+        })
+        .fail(function(request, textStatus, errorThrown){
+          reject(errorThrown);
+        });
+    });
+  }
+
+  function typesPromise(){
+    return new Promise(function(resolve, reject){
+      $.ajax("/javascripts/types.json")
+        .done(function(data){
+          resolve(data);
+        })
+        .fail(function(request, textStatus, errorThrown){
+          reject(errorThrown);
+        });
+    });
+  }
+
+  function productsPromise(){
+    return new Promise(function(resolve, reject){
+      $.ajax("/javascripts/products.json")
+        .done(function(data){
+          resolve(data);
+        })
+        .fail(function(request, textStatus, errorThrown){
+          reject(errorThrown);
+        });
+    });
+  }
+
+  productInfo.getJSONFile = function(){
+    categoriesPromise().then(function(catData){
+      categories = catData;
+      console.log(categories);
+      return typesPromise();
+    }).then(function(typesData){
+      types = typesData;
+      console.log(types);
+      return productsPromise();
+    }).then(function(prodData){
+      products = prodData;
+      console.log(products);
+    });
+  };
+
+  return productInfo;
+
+})(productInfo || {});
---
+++ javascripts/main.js
@@ -0,0 +1,3 @@
+"use strict";
+
+productInfo.getJSONFile();
